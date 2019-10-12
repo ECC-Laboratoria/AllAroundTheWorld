35cda96000e615a3213036ac5822f718efbdf889
nestorivanmo <nestor.martinez.98@hotmail.com>
2019-10-12 13:11:56 -0500

    Added uinavigation controller example

---
+++ Week3/ExamplesInClass/UINavigationController_ECC/UINavigationController_ECC/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UINavigationController_ECC
+//
+//  Created by Néstor I. Martínez Ostoa on 12/10/19.
+//  Copyright © 2019 Néstor I. Martínez Ostoa. All rights reserved.
+//
+
+import UIKit
+
+//Red View Controller
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func returnToRedScreen(segue: UIStoryboardSegue) {
+        
+    }
+
+}
+
